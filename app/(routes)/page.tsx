9eb631a3e312b9da252928edf2d48733acfb3294
simontcodes <simont.codes@gmail.com>
2024-06-07 12:09:57 -0500

    single product page finished

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,24 +6,21 @@ import ProductList from "../components/product-list";
 
 export const revalidate = 0;
 
-
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
 
-    const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("a13c6e2b-ca1d-4488-9587-2b303eb40b05");
+  return (
+    <Container>
+      <div className=" space-y-10 pb-10">
+        <Billboard data={billboard} />
 
-    const billboard = await getBillboard("a13c6e2b-ca1d-4488-9587-2b303eb40b05")
-    return(
-        <Container>
-            <div className=" space-y-10 pb-10">
-                <Billboard data={billboard} />
-            </div>
-
-            <div className=" flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                <ProductList title="Featured Products" items={products}  />
-
-            </div>
-        </Container>
-    )
-}
+        <div className=" flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default HomePage;
--- app/components/ui/icon-button.tsx
+++ app/components/ui/icon-button.tsx
@@ -15,7 +15,7 @@ const IconButton: React.FC<IconButtonProps> = ({ onClick, icon, className }) => 
         "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
         className
       )}
-    ></button>
+    >{icon}</button>
   );
 };
 
