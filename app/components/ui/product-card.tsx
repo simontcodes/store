c6e617d68d4bd1b105eb25e17ca2ec85ecadb91b
simontcodes <simont.codes@gmail.com>
2024-06-11 23:39:54 -0500

    modal finished with bugs

--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -6,18 +6,26 @@ import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
-
+  const previewModal = usePreviewModal();
   const router = useRouter();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data)
+  }
 
   return (
     <div onClick={handleClick} className=" bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -32,7 +40,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className=" opacity-0 group-hover:opacity-100 transition absolute w-full p-6 bottom-5">
           <div className=" flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
+import { Modak, Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import ModalProvider from "@/providers/modal-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
